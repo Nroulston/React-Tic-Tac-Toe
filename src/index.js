65fb621c689dac1ec18449fee9a8e2c31253dfdb
Nroulston <nicolas.roulston@gmail.com>
2020-11-02 10:59:11 -0700

    fix error 0 is undefined

--- src/index.js
+++ src/index.js
@@ -17,19 +17,25 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      squares: Array(9).fill(null)
-    }
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   renderSquare(i) {
     return (
-      <Square 
+      <Square
+        value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
-        value={this.state.square[i]} 
       />
-    )
+    );
   }
 
   render() {
@@ -39,19 +45,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
